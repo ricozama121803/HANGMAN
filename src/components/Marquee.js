e01d463f4f7bcf01f773705a51b38d980e33543f
Enrico Zamarripa <ricozama121803@gmail.com>
2024-11-21 23:24:45 -0800

    Fix: Escape quotes in Marquee component

--- src/components/Marquee.js
+++ src/components/Marquee.js
@@ -3,10 +3,10 @@ export default function Marquee() {
       <div className="fixed top-0 left-0 right-0 bg-red-500 text-white py-3 z-50">
         <div className="flex whitespace-nowrap overflow-hidden">
           <div className="animate-marquee inline-block">
-            No more guesses, click "New Word" to play again! &nbsp;
+            No more guesses, click &quot;New Word&quot; to play again! &nbsp;
           </div>
           <div className="animate-marquee inline-block">
-            No more guesses, click "New Word" to play again! &nbsp;
+            No more guesses, click &quot;New Word&quot; to play again! &nbsp;
           </div>
         </div>
       </div>
